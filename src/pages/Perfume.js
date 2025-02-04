bb40c1dff63443cb3ee985300971b99633b15146
mcy0325 <mcy325@naver.com>
2025-02-04 22:22:02 +0900

    add: create Perfume.js

---
+++ src/pages/Perfume.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../components/Banner";
+
+const Perfume = () => {
+  return (
+    <div>
+      <Banner title="Perfume" imagePath={"/banner_perfume.png"} />
+    </div>
+  );
+};
+
+export default Perfume;
