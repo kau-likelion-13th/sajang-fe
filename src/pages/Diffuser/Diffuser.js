be2ea3b5944f9cd08c90fed6bf3ab434e4508544
mcy0325 <mcy325@naver.com>
2025-02-06 21:34:34 +0900

    add: add banner at Diffuser.js

---
+++ src/pages/Diffuser/Diffuser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/Banner";
+
+const Diffuser = () => {
+  return (
+    <div>
+      <Banner title="Diffuser" imagePath={"/banner_diffuser.png"} />
+    </div>
+  );
+};
+
+export default Diffuser;
