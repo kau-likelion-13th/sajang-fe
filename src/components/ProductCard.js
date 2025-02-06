41f02a38e1ebccc3c895c45b7dc69d594002efc4
mcy0325 <mcy325@naver.com>
2025-02-06 21:33:16 +0900

    add: ProductCard.js, ProductCard.css

---
+++ src/components/ProductCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./../styles/ProductCard.css";
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card">
+      {product.isNew && <div className="new-badge">New</div>}
+      <img
+        src={product.imagePath}
+        alt={product.name}
+        className="product-image"
+      />
+      <div className="product-name">{product.name}</div>
+      <div className="product-brand">{product.brand}</div>
+      <div className="product-price">{product.price}</div>
+    </div>
+  );
+};
+
+export default ProductCard;
