126917edf80bf17c3610a64744438bd37bb7f232
mcy0325 <mcy325@naver.com>
2025-02-06 21:34:00 +0900

    add: add Banner at New.js

---
+++ src/pages/New/New.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/Banner";
+
+const New = () => {
+  return (
+    <div>
+      <Banner title="New" imagePath={"/banner_new.png"} />
+    </div>
+  );
+};
+
+export default New;
