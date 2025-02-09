9e55f2bbe6c9154841c0f6d4e69283b965a3ef74
mcy0325 <mcy325@naver.com>
2025-02-09 21:50:21 +0900

    modify: New.js

--- src/pages/ProductPage/New.js
+++ src/pages/ProductPage/New.js
@@ -1,10 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./Banner";
+import ProductCard from "./ProductCard";
+import "../../styles/ProductPage.css";
 
 const New = () => {
+  const products = [
+    {
+      id: 1,
+      name: "벚꽃 디퓨저",
+      brand: "코코도르",
+      price: "40,000 원",
+      imagePath: "/img/diffuser_1.png",
+      isNew: true,
+    },
+    {
+      id: 2,
+      name: "벚꽃 디퓨저",
+      brand: "코코도르",
+      price: "40,000 원",
+      imagePath: "/img/diffuser_2.png",
+      isNew: true,
+    },
+    {
+      id: 3,
+      name: "벚꽃 디퓨저",
+      brand: "코코도르",
+      price: "40,000 원",
+      imagePath: "/img/diffuser_3.png",
+      isNew: true,
+    },
+    {
+      id: 4,
+      name: "벚꽃 디퓨저",
+      brand: "코코도르",
+      price: "40,000 원",
+      imagePath: "/img/diffuser_4.png",
+      isNew: true,
+    },
+    {
+      id: 5,
+      name: "벚꽃 디퓨저",
+      brand: "코코도르",
+      price: "40,000 원",
+      imagePath: "/img/diffuser_5.png",
+      isNew: true,
+    },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 15;
+
+  const totatlPages = Math.ceil(products.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentProducts = products.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <Banner title="New" imagePath={"/banner_new.png"} />
+      <div className="product-container">
+        <div className="product-grid">
+          {currentProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+        <div className="paging">
+          {currentPage > 1 && (
+            <button onClick={() => handlePageChange(currentPage - 1)}>
+              Prev
+            </button>
+          )}
+          {Array.from({ length: totatlPages }, (_, i) => i + 1).map(
+            (pageNumber) => (
+              <button
+                key={pageNumber}
+                onClick={() => handlePageChange(pageNumber)}
+                className={pageNumber === currentPage ? "active" : ""}
+              >
+                {pageNumber}
+              </button>
+            )
+          )}
+          {currentPage < totatlPages && (
+            <button onClick={() => handlePageChange(currentPage + 1)}>
+              Next
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
