a2c2a389e2cc01a642236a82234c79c76e981e09
mcy0325 <mcy325@naver.com>
2025-02-10 00:26:10 +0900

    modify: PayModal.js, PayModal.css

---
+++ src/components/PayModal.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import "../styles/PayModal.css";
+
+const PayModal = ({ product, onClose }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [mileage, setMileage] = useState("");
+  const maxMileage = 100000;
+  const [productPrice, setProductPrice] = useState(product.price);
+  const [totalPrice, setTotalPrice] = useState(product.price);
+  const handleQuantityChange = (type) => {
+    setQuantity((prev) => (type === "plus" ? prev + 1 : Math.max(1, prev - 1)));
+  };
+
+  useEffect(() => {
+    const newProductPrice = product.price * quantity;
+    setProductPrice(newProductPrice);
+    setTotalPrice(Math.max(newProductPrice - mileage, 0));
+  }, [quantity, mileage, product.price]);
+
+  const handleMileageChange = (e) => {
+    const value = e.target.value;
+    const numericValue = value === "" ? 0 : Math.min(Number(value), maxMileage);
+    setMileage(numericValue);
+  };
+
+  return (
+    <div className="modal">
+      <div className="overlay" onClick={onClose}></div>
+      <div className="container">
+        <button className="close-button" onClick={onClose}>
+          &times;
+        </button>
+        <div className="title">주문 / 결제</div>
+
+        <div className="section">
+          <div className="section-title">주문 상품</div>
+          <div className="order-info">
+            <img
+              src={product.imagePath}
+              alt={product.name}
+              className="order-image"
+            />
+            <div>
+              <div className="order-name">{product.name}</div>
+              <div className="order-brand">{product.brand}</div>
+              <div className="order-price">
+                {product.price.toLocaleString()} 원
+              </div>
+              <div className="quantity-control">
+                <button
+                  className="quantity-button"
+                  onClick={() => handleQuantityChange("minus")}
+                >
+                  -
+                </button>
+                <div className="quantity">{quantity}</div>
+                <button
+                  className="quantity-button"
+                  onClick={() => handleQuantityChange("plus")}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="section-title">배송지</div>
+          <div className="user-info">아리</div>
+          <div className="user-info">010-0000-0000</div>
+          <div className="user-info">
+            경기도 고양시 덕양구 항공대학로 76 국제은익관 1생활관 F000
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="section-title">마일리지 사용</div>
+          <div className="mileage-info">
+            현재 사용 가능한 마일리지: {maxMileage.toLocaleString()} 원
+          </div>
+          <input
+            className="mileage-input"
+            placeholder="사용하실 마일리지를 입력하세요"
+            value={mileage}
+            onChange={handleMileageChange}
+          />
+        </div>
+
+        <div className="section">
+          <div className="section-title">총 결제금액</div>
+          <div className="total">
+            <div>
+              <div className="total-item">총 상품금액</div>
+              <div className="total-item">마일리지 할인</div>
+              <div className="total-item">배송비</div>
+            </div>
+            <div>
+              <div className="total-value">
+                {totalPrice.toLocaleString()} 원
+              </div>
+              <div className="total-value discount">
+                -{mileage.toLocaleString()} 원
+              </div>
+              <div className="total-value">무료배송</div>
+            </div>
+          </div>
+        </div>
+
+        <button className="pay-button">결제하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default PayModal;
